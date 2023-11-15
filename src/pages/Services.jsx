562d4991363d8067cd067b61f04995c8a17d7717
Edgaras Austinskas <eaustinskas@gmail.com>
2023-11-15 08:32:39 +0200

    Added "Services" content

---
+++ src/pages/Services.jsx
@@ -0,0 +1,40 @@
+// Services.jsx
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div className="services">
+      <div className="service-cards">
+        <div className="ad-services-card card">
+          <div className="card-text">
+            <p>
+              A Service list provides a catalogue of the services that are delivered by an organisation. It is sometimes
+              referred to as a Service Catalogue or a Product Catalogue. A Service defines what is delivered by one
+              organisation, department or individual to another.
+            </p>
+          </div>
+        </div>
+        <div className="beauty-services-card card">
+          <div className="card-text">
+            <p>
+              A Service list provides a catalogue of the services that are delivered by an organisation. It is sometimes
+              referred to as a Service Catalogue or a Product Catalogue. A Service defines what is delivered by one
+              organisation, department or individual to another.
+            </p>
+          </div>
+        </div>
+        <div className="counseling-services-card card">
+          <div className="card-text">
+            <p>
+              A Service list provides a catalogue of the services that are delivered by an organisation. It is sometimes
+              referred to as a Service Catalogue or a Product Catalogue. A Service defines what is delivered by one
+              organisation, department or individual to another.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
